082b74a04083a4abba1e3ad2a34edfe6d2007114
amoreno16003 <alex.moreno161100@gmail.com>
2022-08-06 02:37:04 -0500

    90% Done

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ function getRandomInt(min, max){
     return (Math.floor(Math.random() * (max-min)+min));
 }
 
+function on() {
+    document.getElementById("startScreen").style.display = "block";
+  }
+  
+function off() {
+    document.getElementById("startScreen").style.display = "none";
+    gamePaused = false;
+}
+
 //GLOBAL VARIABLES
 var game = document.getElementById('game');
 const roundNumContent = document.getElementById('roundNum');
@@ -9,6 +18,7 @@ const damageLevelContent = document.getElementById('damageLevel')
 const killCount = document.getElementById('kill-count');
 const healthLevelContent = document.getElementById('healthLevel');
 const ctx = game.getContext('2d');
+const startButton = document.getElementById('start-button');
 console.log(game.width+" "+game.height);
 
 let rambo; //Main Character
@@ -21,9 +31,44 @@ let totalKills = 0;
 achiliesFacingLeft = document.getElementById("AFL");
 Enemy1Right1 = document.getElementById("Enemy1Right1");
 
+//mages
+
+//back frames
+MageBackFrame1 = document.getElementById("MageBackFrame1");
+MageBackFrame2 = document.getElementById("MageBackFrame2");
+MageBackFrame3 = document.getElementById("MageBackFrame3");
+MageBackFrame4 = document.getElementById("MageBackFrame4");
+let MageBackFramesArr = [MageBackFrame1, MageBackFrame2, MageBackFrame3, MageBackFrame4]
+//front frames
+MageFrontFrame1 = document.getElementById("MageFrontFrame1");
+MageFrontFrame2 = document.getElementById("MageFrontFrame2");
+MageFrontFrame3 = document.getElementById("MageFrontFrame3");
+MageFrontFrame4 = document.getElementById("MageFrontFrame4");
+let MageFrontFramesArr = [MageFrontFrame1, MageFrontFrame2, MageFrontFrame3, MageFrontFrame4];
+//left frames
+MageLeftFrame1 = document.getElementById("MageLeftFrame1");
+MageLeftFrame2 = document.getElementById("MageLeftFrame2");
+MageLeftFrame3 = document.getElementById("MageLeftFrame3");
+MageLeftFrame4 = document.getElementById("MageLeftFrame4");
+let MageLeftFramesArr = [MageLeftFrame1, MageLeftFrame2, MageLeftFrame3, MageLeftFrame4]
+//right frames
+MageRightFrame1 = document.getElementById("MageRightFrame1");
+MageRightFrame1 = document.getElementById("MageRightFrame2");
+MageRightFrame1 = document.getElementById("MageRightFrame3");
+MageRightFrame1 = document.getElementById("MageRightFrame4");
+let MageRightFramesArr = [MageRightFrame1, MageRightFrame2, MageRightFrame3, MageRightFrame4]
+
+
+//zombies
+ZombieBackFrame1 = document.getElementById("ZombieBackFrame1");
+ZombieFrontFrame1 = document.getElementById("ZombieFrontFrame1");
+ZombieLeftFrame1 = document.getElementById("ZombieLeftFrame1");
+ZombieRightFrame1 = document.getElementById("ZombieRightFrame1");
+
 //Game Progression
 let roundNum = 1;
 var arrayOfBullets = [];
+let gamePaused = true;
 
 //CLASSES
  
@@ -149,15 +194,21 @@ function delay(arr, i) {
 //Starting Screen
 document.addEventListener('keydown', movementHandler);
 window.addEventListener('DOMContentLoaded', function() {
-    rambo = new Hero((game.width/2)-12.75, (game.height/2)-16, 25.5, 32, 'blue', achiliesFacingLeft);
+    rambo = new Hero((game.width/2)-16, (game.height/2)-13.25, 32, 26.5, 'blue', MageFrontFrame1); //32 53
     //rambo = new Hero(achiliesFacingLeft, game.width/2, game.height/2);
     roundNumContent.textContent = `Round: ${roundNum}`;
-    const runningGame = this.setInterval(gameLoop, 60);
+    const runningGame = this.setInterval(function() {
+        if (gamePaused !== true){
+            gameLoop();
+        } 
+    }, 60);
+    
     
 });
 
 ///MOVEMENT HANDLER
 let i = 0;
+let attackSequenceFrameCounter = 1;
 let testBullet = new Bullet(game.width/2, game.height/2, 1, 1, "yellow");
 testBullet.alive = false;
 arrayOfBullets.push(testBullet);
@@ -170,7 +221,18 @@ function movementHandler(event){
         
         if (event.key === "ArrowUp"){
             arrayOfBullets.push(new Bullet(game.width/2, game.height/2, 1, 3, "yellow"));
-            
+            rambo.image = MageBackFrame1;
+            let frontAttackSequence = setInterval(function() {
+                rambo.image = MageBackFramesArr[attackSequenceFrameCounter];
+                //console.log(attackSequenceFrameCounter);
+                rambo.render();
+                attackSequenceFrameCounter++;
+                if (attackSequenceFrameCounter >= 4){
+                    rambo.image = MageBackFrame1;
+                    attackSequenceFrameCounter = 1;
+                    clearInterval(frontAttackSequence);
+                }
+            }, 75)
             let upInterval = setInterval(function() {
                 arrayOfBullets[i].moveBulletUp();
                 arrayOfBullets[i].render();
@@ -183,6 +245,18 @@ function movementHandler(event){
 
         else if (event.key === "ArrowRight"){
             arrayOfBullets.push(new Bullet(game.width/2, game.height/2, 5, 1, "red"));
+            rambo.image = MageRightFrame1;
+            let frontAttackSequence = setInterval(function() {
+                rambo.image = MageRightFramesArr[attackSequenceFrameCounter];
+                //console.log(attackSequenceFrameCounter);
+                rambo.render();
+                attackSequenceFrameCounter++;
+                if (attackSequenceFrameCounter >= 4){
+                    rambo.image = MageRightFrame1;
+                    attackSequenceFrameCounter = 1;
+                    clearInterval(frontAttackSequence);
+                }
+            }, 75)
             let rightInterval = setInterval(function() {
                 arrayOfBullets[i].moveBulletRight();
                 if (arrayOfBullets[i].alive === false) {
@@ -193,6 +267,18 @@ function movementHandler(event){
 
         else if (event.key === "ArrowDown"){
             arrayOfBullets.push(new Bullet(game.width/2, game.height/2, 1, 3, "yellow"));
+            //rambo.image = MageFrontFrame1;
+            let frontAttackSequence = setInterval(function() {
+                rambo.image = MageFrontFramesArr[attackSequenceFrameCounter];
+                //console.log(attackSequenceFrameCounter);
+                rambo.render();
+                attackSequenceFrameCounter++;
+                if (attackSequenceFrameCounter >= 4){
+                    rambo.image = MageFrontFrame1;
+                    attackSequenceFrameCounter = 1;
+                    clearInterval(frontAttackSequence);
+                }
+            }, 75)
             let downInterval = setInterval(function() {
                 arrayOfBullets[i].moveBulletDown();
                 if (arrayOfBullets[i].alive === false) {
@@ -203,6 +289,18 @@ function movementHandler(event){
 
         else if (event.key === "ArrowLeft"){
             arrayOfBullets.push(new Bullet(game.width/2, game.height/2, 5, 1, "red"));
+            rambo.image = MageLeftFrame1;
+            let frontAttackSequence = setInterval(function() {
+                rambo.image = MageLeftFramesArr[attackSequenceFrameCounter];
+                //console.log(attackSequenceFrameCounter);
+                rambo.render();
+                attackSequenceFrameCounter++;
+                if (attackSequenceFrameCounter >= 4){
+                    rambo.image = MageLeftFrame1;
+                    attackSequenceFrameCounter = 1;
+                    clearInterval(frontAttackSequence);
+                }
+            }, 75)
             let leftInterval = setInterval(function() {
                 // console.log(i);
                 // console.log(arrayOfBullets[i].alive)
@@ -226,23 +324,31 @@ let enemyArr = [];
 let startingPositions = ['Top', 'Right', 'Bottom', 'Left'];
 let roundOneArr = [];
 for (let i = 0; i < 5; i++){
-    roundOneArr.push(new Enemy1(game.width-20, game.height/2-10, 16, 16, "purple", Enemy1Right1));
+    roundOneArr.push(new Enemy1(game.width-20, game.height/2-10, 32, 26.5, "purple", Enemy1Right1));
     roundOneArr[i].startPos = startingPositions[getRandomInt(0, 4)];
     if (roundOneArr[i].startPos === 'Top'){
-        roundOneArr[i].x = game.width/2-10;
+        roundOneArr[i].x = game.width/2-16;
         roundOneArr[i].y = 0;
+        roundOneArr[i].image = ZombieFrontFrame1;
+
     }
     else if (roundOneArr[i].startPos === 'Right'){
         roundOneArr[i].x = game.width-20;
-        roundOneArr[i].y = game.height/2-10;
+        roundOneArr[i].y = game.height/2-13.25;
+        roundOneArr[i].image = ZombieLeftFrame1;
+
     }
     else if (roundOneArr[i].startPos === 'Bottom'){
-        roundOneArr[i].x = game.width/2-10;
+        roundOneArr[i].x = game.width/2-16;
         roundOneArr[i].y = game.height-20;
+        roundOneArr[i].image = ZombieBackFrame1;
+
     }
     else if (roundOneArr[i].startPos === 'Left'){
         roundOneArr[i].x = 0;
-        roundOneArr[i].y = game.height/2-10;
+        roundOneArr[i].y = game.height/2-13.25;
+        roundOneArr[i].image = ZombieRightFrame1;
+
     }
 }
 console.log(roundOneArr);
@@ -285,20 +391,24 @@ function gameLoop(){
     
     setTimeout(function(){
         for (let i = 0; i < roundOneArr.length; i++){
+            if (!detectHit(roundOneArr[i], rambo)){
             if (roundOneArr[i].rendered === true){
                 if (roundOneArr[i].startPos === 'Top'){
-                    roundOneArr[i].y += 0.5;
+                        roundOneArr[i].y += 0.5;
                 }
                 else if (roundOneArr[i].startPos === 'Right'){
                     roundOneArr[i].x -= 0.5;
                 }
                 else if (roundOneArr[i].startPos === 'Bottom'){
+                    
                     roundOneArr[i].y -= 0.5;
+                
                 }
                 else if (roundOneArr[i].startPos === 'Left'){
-                    roundOneArr[i].x += 0.5;
+                        roundOneArr[i].x += 0.5;
                 }
             }
+        }
             
         }
          
@@ -312,6 +422,7 @@ function gameLoop(){
             if (health <= 0){
                 //alert("GAME OVER");
                 //break;
+                document.getElementById("gameover-screen").style.display = "block";
             }
         }
         if (arrayOfBullets[arrayOfBullets.length-1].collision === 0){
